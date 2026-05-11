697bfd12fb8f20b5355a5988a3ff010cc073c8a2
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2026-05-11 19:42:56 +0500

    get and post methods

--- app/movies/route.ts
+++ app/movies/route.ts
@@ -1,5 +1,12 @@
 import { movies } from "./db";
 export async function GET() {
-  return;
-  Response.json(movies);
+  return Response.json(movies);
+}
+
+export async function POST(request: Request) {
+  let movie = await request.json();
+  console.log("New movie added:", movie);
+  const newMovie = { ...movie, id: movies.length + 1 };
+  movies.push(newMovie);
+  return new Response(JSON.stringify(newMovie), { status: 201 });
 }
