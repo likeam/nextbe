4f45fe013cdced626cf88bd99e5fa107ec414b71
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2026-05-12 22:01:35 +0500

    Building Patching in BE

--- app/movies/[id]/route.ts
+++ app/movies/[id]/route.ts
@@ -6,3 +6,29 @@ export async function GET(_req: Request, { params }: Props) {
     ? new Response(JSON.stringify(movie), { status: 200 })
     : new Response("Movie not found", { status: 404 });
 }
+
+export async function PATCH(_req: Request, { params }: Props) {
+  const { id } = await params;
+  const movie = movies.find((movie) => movie.id === +id);
+  if (!movie) {
+    return new Response("Movie not found", { status: 404 });
+  }
+
+  const body = await req.json();
+
+  const updateTitle = body.title || movie.title;
+  const updateDirector = body.director || movie.director;
+  const updateYear = body.year || movie.year;
+  const updateGenre = body.genre || movie.genre;
+
+  return new Response(
+    JSON.stringify({
+      ...movie,
+      title: updateTitle,
+      director: updateDirector,
+      year: updateYear,
+      genre: updateGenre,
+    }),
+    { status: 200 },
+  );
+}
